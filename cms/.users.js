670557c529dc6c8c785bb5969db479a6469375fd
Diego Ronchieri <dronchieri@motormax.com.ar>
2018-08-14 15:05:10 -0300

    first commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'fsdesign',
+			tags: [],
+			salt: '7765d4d17ac450a3761d5adcc0599ad4',
+			hash: 'b27f670f0b0cef962d4fe9b74ae26e1b0a7ac72c4378895a5380106285d4f307',
+			user_created_timestamp: 1534256314731
+		}
+	]
+})
---
+++ cms/index.js
@@ -0,0 +1,60 @@
+({
+	products: [
+		{
+			name: 'Producto 1',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: true
+		},
+		{
+			name: 'Producto 2',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: true
+		},
+		{
+			name: 'Producto 3',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: true
+		},
+		{
+			name: 'Producto 4',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: true
+		},
+		{
+			name: 'Producto 4',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: false
+		},
+		{
+			name: 'Producto 4',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: false
+		},
+		{
+			name: 'Producto 4',
+			description: 'By adding a file to /pages folder',
+			price: '$600',
+			link: 'https://www.endurojs.com/docs/templates',
+			image: 'lphdp4.jpg',
+			publish: false
+		}
+	]
+})
